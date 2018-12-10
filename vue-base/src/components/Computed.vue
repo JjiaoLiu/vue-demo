e6de6214ab48bb405a2572593c6e9ffc13977111
刘娇娇 <1019224103@qq.com>
2018-12-10 20:37:32 +0800

    computed

--- vue-base/src/components/Computed.vue
+++ vue-base/src/components/Computed.vue
@@ -1,8 +1,11 @@
 <template>
   <section>
+    {{miniOption}}
     <input type="text" v-model="firstName">
     <input type="text" v-model="lastName">
     <input type="text" v-model="fullName">
+    <button @click="changeOption">修改option</button>
+    <button @click="changeMini">修改mini</button>
   </section>
 </template>
 
@@ -13,6 +16,16 @@
       return {
         firstName: "liu",
         lastName: "jiaojiao",
+        option: [{value: 1}, {value: 2}, {value: 3}, {value: 4}],
+        optionMini: [1, 2, 3, 4, 5]
+      }
+    },
+    methods: {
+      changeOption() {
+        this.option.push({value: 5})
+      },
+      changeMini() {
+        this.optionMini.splice(2, 1)
       }
     },
     computed: {
@@ -21,10 +34,15 @@
           return this.firstName + "-" + this.lastName
         },
         set(newValue) {
-          var names = newValue.split("-")
-          this.firstName = names[0]
+          let names = newValue.split("-");
+          this.firstName = names[0];
           this.lastName = names[1]
         }
+      },
+      miniOption() {
+        return this.option.filter(f => {
+          return this.optionMini.includes(f.value)
+        })
       }
     }
   }
